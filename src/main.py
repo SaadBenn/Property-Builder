ef678093145716b39b2cc92c44232b88ef79ba43
Saad Benn <saadbennmushtaq@gmail.com>
2018-11-06 19:27:52 -0500

    Test communication with website

--- src/main.py
+++ src/main.py
@@ -4,4 +4,19 @@ import pandas as pd
 import itertools
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 sns.set()
+
+"""
+Header to pass along the get command just to make the queries look like
+they are actually coming from an actual browser.
+"""
+headers = ({'User-Agent':
+            'Mozilla/5.0 (Windows NT 6.1) AppleWebkit/537.36 (KHTML, '
+            'like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+
+url = "https://casa.sapo.pt/Venda/Apartmentos/?sa=11&or=10"
+response = get(url, headers=headers)
+
+print(response)
+print(response.text[:1000])
